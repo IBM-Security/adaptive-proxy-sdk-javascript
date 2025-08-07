52c80e10fa499e65bfb1d4352ee2db48d3431c62
Konstantin Trofimov <konstantin.trofimov@ibm.com>
2025-08-07 15:00:17 +1000

    Revert "updated README.md with the new URL for this repository"
    This reverts commit 9c6b4e6eb0dbf479ae0e5f54405e611cb6769660.

---
+++ demo/components/a2/adaptive.js
@@ -0,0 +1,13 @@
+// Import the Adaptive SDK.
+const Adaptive = require('@ibm-verify/adaptive-proxy');
+
+// Load contents of `.env` into `process.env`.
+require('dotenv').config();
+
+const config = {
+  tenantUrl: process.env.TENANT_URL,
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
+};
+
+module.exports = new Adaptive(config);
